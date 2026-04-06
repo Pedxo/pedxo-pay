69cdeabaa944330511117f3c8887cc137ee7a4c6
Israel-Adekanmi <adekanmiisrael8@gmail.com>
2026-04-06 20:23:44 +0100

    feat: cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "https://pedxo.com",
+      "https://pedxo-back-project.onrender.com", 
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT);
 }
 
